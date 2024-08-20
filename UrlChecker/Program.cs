90b1fe9822563b7542b233c219f66302a74721df
YuMingChan <136806536+YuMingChan@users.noreply.github.com>
2024-08-20 14:49:09 +0100

    Add files via upload

---
+++ UrlChecker/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var blockedUrls = new List<string> { "blocked.com", "test.com" };
+        var toCheckUrls = new List<string> { "https://notblocked.com", "https://subdomain.blocked.com/path", "https://blocked.com", "https://test.com" };
+
+        var urlChecker = new UrlChecker.UrlChecker(blockedUrls);
+        var filteredUrls = urlChecker.FilterBlockedUrls(toCheckUrls);
+
+        foreach (var url in filteredUrls)
+        {
+            Console.WriteLine(url);
+        }
+        Console.ReadLine ();
+    }
+}
---
+++ UrlChecker/UrlChecker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UrlChecker
+{
+    public class UrlChecker
+    {
+        private readonly List<string> blockedUrls;
+        private readonly char[] specialChars;
+
+        public UrlChecker(List<string> blockedUrls)
+        {
+            this.blockedUrls = blockedUrls;
+            specialChars = Enumerable.Range(0, 65535)
+                                     .Select(i => (char)i)
+                                     .Where(c => !char.IsLetterOrDigit(c) && !char.IsWhiteSpace(c))
+                                     .ToArray();
+        }
+
+        public bool IsBlocked(string url)
+        {
+            foreach (var blockedUrl in blockedUrls)
+            {
+           
+                int index = url.IndexOf(blockedUrl);
+                if (index >= 0) // Found the blocked URL
+                {
+                    int nextCharIndex = index + blockedUrl.Length;
+                    if (specialChars.Contains(url[index - 1]) &&(nextCharIndex >= url.Length || specialChars.Contains(url[nextCharIndex]))  )
+                    {
+                        return true; // Blocked if no character or special character before or follows
+                    }
+                  
+                }
+
+               
+            }
+            return false;
+        }
+        public List<string> FilterBlockedUrls(List<string> toCheckUrls)
+        {
+            return toCheckUrls.Where(url => !IsBlocked(url)).ToList();
+        }
+    }
+}
